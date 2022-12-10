57b1143255ea86a3c80650bfe48771cc7d44b80c
bo7owers <7orres.rene@proton.me>
2022-12-09 20:32:34 -0500

    Style btn

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,17 @@ const sortGifts = computed(() => [...presents].sort(modes[state.value]))
 </script>
 
 <template>
+  <p>
+    made by
+    <a
+      href="http://github.com/bo7owers"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="underline rounded mx-auto text-lime-700 hover:text-cyan-800 .focus:text-cyan-800 transition-colors"
+      title="opens in a new tab"
+      >bo7owers</a
+    >
+  </p>
   <div class="w-full h-full p-4 flex justify-center items-center">
     <div class="max-w-md">
       <div>
@@ -30,7 +41,11 @@ const sortGifts = computed(() => [...presents].sort(modes[state.value]))
           :data-surface="present.dimensions.height * present.dimensions.width"
         />
       </div>
-      <button @click="next()">Toggle Sort</button>
+      <div class="flex justify-center mt-5">
+        <button @click="next()" class="rounded-full bg-purple-600 text-white p-2 hover:bg-purple-900">
+          Toggle Sort
+        </button>
+      </div>
     </div>
   </div>
 </template>
